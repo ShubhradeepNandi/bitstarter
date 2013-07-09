28e04269b0c4a4526ea7a98a5e1e59fffd9affa4
Shubhradeep <shubhradeep@Banshankari-Satellite-L300.(none)>
2013-07-10 01:22:30 +0530

    added html to js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2');
+fs.read('index.html',function(err,data){
+ if(err)throw err;
+   response.send(data.toString("utf-8"));})
 });
 
 var port = process.env.PORT || 5000;
